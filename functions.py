4e3e195018bb6162395aa926ed018dbc23924f08
Jeremy Roy <jezoffy@gmail.com>
2023-10-30 13:32:19 +0000

    refresh

--- functions.py
+++ functions.py
@@ -4,25 +4,46 @@ import os
 import sys
 from datetime import datetime
 
-def login_required(f):
+def RequireUser(f):
     """
     Decorate routes to require login. Prevents unauthorised and non-contextual access.
     """
     @wraps(f)
-    def decorated_function(*args, **kwargs):
+    def decorate(*args, **kwargs):
         if session.get("username") is None:
+            # Stops a non-logged-in user from accessing parts of the website and redirects them.
             return redirect("/login")
         return f(*args, **kwargs)
-    return decorated_function
+    return decorate
 
-def updateLog(action):
+def UpdateLog(action):
     """
     Updates the server log with everything that happens in the app. 
     'action' is a string variable.
     """
     now = datetime.now()
     myFile = open("server.log", "a")
-    contents = str(now)[:18]
-    print(contents)
+    contents = f"{str(now)[:19]}: {action}"
+    myFile.write(contents)
     myFile.close()
     return True
+
+def StatusCode(message, code=400):
+    """
+    For error codes and maintaining site consistency even when things go wrong.
+    'message' is a string.
+    'code' refers to context of message.
+    """
+    def escape(s):
+        """
+        Escape special characters.
+        Original material found for this found:
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("error.html", top=code, bottom=escape(message)), code
+    
+UpdateLog("Server Log initialisation/update")
