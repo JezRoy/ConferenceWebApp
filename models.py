d840b25eceb0077364f097ac37aae4dbead1cc80
Jeremy Roy <jezoffy@gmail.com>
2023-11-23 10:49:40 +0000

    database init complete

--- models.py
+++ models.py
@@ -7,40 +7,32 @@ cursor = conn.cursor()
 def initialise():
     """ Initialise the entire database. """
     try:
+        # Delegates
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS delegates
                 id INTEGER PRIMARY KEY,
                 username TEXT NOT NULL,
                 passwordHash TEXT NOT NULL
                        ''')
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS delLikes
-                delegID INTEGER,
-                topicID INTEGER,
-                FOREIGN KEY (delegID) REFERENCES delegates(id),
-                FOREIGN KEY (topicID) REFERENCES tastes(id)
-                       ''')
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS tastes
-                id INTEGER PRIMARY KEY,
-                topic TEXT NOT NULL
-                       ''')
+        # Host users for conferences
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS hosts
                 id INTEGER PRIMARY KEY,
                 username TEXT NOT NULL,
                 passwordHash TEXT NOT NULL
                        ''')
+        # Conferences created
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS conferences
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
                 hostID INTEGER,
                 paperSubDeadline DATE,
-                delegateSignupDeadline DATE,
+                delegateSignUpDeadline DATE,
                 confStart DATE,
                 confEnd DATE
                        ''')
+        # Speakers for the conferences
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS speakers
                 id INTEGER PRIMARY KEY,
@@ -48,6 +40,7 @@ def initialise():
                 delegateID INTEGER,
                 FOREIGN KEY (delegateID) REFERENCES delegates(id)
                        ''')
+        # Talks being given at conferences
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS talks
                 talkID INTEGER PRIMARY KEY,
@@ -57,6 +50,30 @@ def initialise():
                 FOREIGN KEY (speakerID) REFERENCES speakers(id),
                 FOREIGN KEY (confID) REFERENCES conferences(id)
                        ''')
+        # Table for tastes and prefernces
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tastes
+                id INTEGER PRIMARY KEY,
+                topic TEXT NOT NULL,
+                confID INTEGER,
+                FOREIGN KEY (confID) REFERENCES conferences(id)
+                       ''')
+        # Delegates like certain topics
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS delLikes
+                delegID INTEGER,
+                topicID INTEGER,
+                FOREIGN KEY (delegID) REFERENCES delegates(id),
+                FOREIGN KEY (topicID) REFERENCES tastes(id),
+                       ''')
+        # Hosts direct a conference
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS hostConf
+                confID INTEGER,
+                hostID INTEGER,
+                FOREIGN KEY (confID) REFERENCES conferences(id),
+                FOREIGN KEY (hostID) REFERNCES hosts(id)
+                       ''') 
         return True
     except:
         return False
