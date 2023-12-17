739e244f6631d8945bbdd6ca989ef45858d711ab
Jeremy Roy <jezoffy@gmail.com>
2023-12-17 17:13:35 +0000

    a new approach

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from website import CreateApp
 
 ### To be dragged from out of the website folder
+# This web app has been created with help from
+# https://www.youtube.com/watch?v=dam0GPOAvVI&t=288s&ab_channel=TechWithTim
+
+"""During Development
+Set the right python interpreter using:
+- cmd+shift+p
+- 'python select interpreter'
+- python3.8.8 (NOT conda)
+"""
 
 app = CreateApp()
 
