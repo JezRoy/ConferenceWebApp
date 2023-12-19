c8b55431a7b52fbf1d9d4a11034b27aacbc730ad
Jeremy Roy <jezoffy@gmail.com>
2023-12-19 14:53:28 +0000

    continue web app progression

--- website/routesAndviews.py
+++ website/routesAndviews.py
@@ -1,5 +1,20 @@
 from flask import Blueprint, render_template
 
+# Arguments to consider when rendering a template
+"""
+DEFAULTS:
+- When using flash() to flash alerts and warning there are 3 string categories:
+    - error
+    - info
+    - warning
+- {{ session.username }} defines whether a user is signed in.
+- {{ conferenceSigned }} determines if a user is signed up to a conference.
+    if so, then present the option to see that conference.
+    - May require a conference search up beforehand to also pass in the URL
+    to a conference as a link in the navbar:
+        - referred to with {{ conferenceURL }}.
+"""
+
 # Setting up a navigation blueprint for the flask application
 views = Blueprint('views', __name__)
 
