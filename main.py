59873e30910dad8193279b126e05d0b90f8a7c60
Jeremy Roy <jezoffy@gmail.com>
2024-02-13 11:54:11 +0000

    testing new conferences with the dashboard

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from website import CreateApp
+from website import CreateApp, Scheduler
+from multiprocessing import Process
 
 ### To be dragged from out of the website folder
-# This web app has been create d with help from
+# This web app has been created with help from
 # https://www.youtube.com/watch?v=dam0GPOAvVI&t=288s&ab_channel=TechWithTim
 
 """During Development
@@ -11,7 +12,10 @@ Set the right python interpreter using:
 - python3.8.8 (NOT conda)
 """
 
+### The scheduler will be run in parallel
+
 app = CreateApp()
 
 if __name__ == '__main__':
+    parallelScheduler = Process(target=Scheduler)
     app.run(debug=True)
---
+++ website/Scheduler.py
@@ -0,0 +1,5 @@
+"""SCHEDULER PROGRAM
+- Create a base using IP / LP
+- Optimise using Graph Theory
+"""
+from . import db
